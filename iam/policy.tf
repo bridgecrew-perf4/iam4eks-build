2a002368fccc3edf9f86f0694cfcdb6130ffe3da
ue-noma <masaki.ueno.3214@gmail.com>
2020-12-26 21:59:01 +0900

    Update policy.tf

--- iam/policy.tf
+++ iam/policy.tf
@@ -2,8 +2,8 @@ data "aws_iam_policy_document" "this" {
   # EKS Full Access
   statement {
     effect = "Allow"
-    actions = "eks:*"
-    resources = "*"
+    actions = ["eks:*"]
+    resources = ["*"]
   }
 }
 
