90c3d0316895f388f2c40186d26727586b44e3a8
ue-noma <masaki.ueno.3214@gmail.com>
2020-12-31 18:15:18 +0900

    Split policy

--- iam/user.tf
+++ iam/user.tf
@@ -6,7 +6,12 @@ resource "aws_iam_user" "this" {
   }
 }
 
-resource "aws_iam_user_policy_attachment" "this" {
+resource "aws_iam_user_policy_attachment" "this_1" {
   user       = aws_iam_user.this.name
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this_1.arn
+}
+
+resource "aws_iam_user_policy_attachment" "this_2" {
+  user       = aws_iam_user.this.name
+  policy_arn = aws_iam_policy.this_2.arn
 }
